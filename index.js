9bab1c0cc92d720597195d0d6d54064fde24460d
annewaithaka <annewaithaka68@gmail.com>
2024-07-06 22:45:31 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    // Select the <p> element
+    const p = document.querySelector('p');
+    
+    // Update its text content
+    p.textContent = "This is really cool!";
+  });
+  
